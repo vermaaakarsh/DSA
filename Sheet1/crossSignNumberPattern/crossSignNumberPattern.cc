20fe69131670c33f01596d7d0aaabb4b96fad1ab
master <myselfmrv@gmail.com>
2022-01-12 22:34:48 +0530

    Sheet1 final commit.

--- Sheet1/crossSignNumberPattern/crossSignNumberPattern.cc
+++ Sheet1/crossSignNumberPattern/crossSignNumberPattern.cc
@@ -1,13 +1,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n,i=1,l=1;//number of lines
+    int n,i;//number of lines
     cin>>n;
-    for(i=(n+1)/2;i>=1;i--){
+    for(i=1;i<=(n+1)/2;i++){
         for(int j=i;j>1;j--){
             cout<<" ";
         }
-        cout<<l;
+        cout<<i;
         for(int j=(n+1)/2;j>i;j--){
             cout<<" ";
         }
@@ -15,24 +15,21 @@ int main(){
             cout<<" ";
         }
         if(i!=(n+1)/2)
-            cout<<l;
+            cout<<i;
         cout<<endl;
-        l++;
     }
-    l-=2;
-    for(i=2;i<=(n+1)/2;i++){
-        for(int j=i;j>1;j--){
+    for(int k=n/2;k>=1;k--){
+        for(int j=k;j>1;j--){
             cout<<" ";
         }
-        cout<<l;
-        for(int j=(n+1)/2;j>i;j--){
+        cout<<i;
+        for(int j=(n+1)/2;j>k;j--){
             cout<<" ";
         }
-        for(int j=(n+1)/2;j>i+1;j--){
+        for(int j=(n+1)/2;j>k+1;j--){
             cout<<" ";
         }
-        if(i!=(n+1)/2)
-            cout<<l--;
+        cout<<i++;
         cout<<endl;
     }
     return 0;
