e73d6a08ad9f2965252f17db5630379ae708b2d7
master <myselfmrv@gmail.com>
2022-01-13 20:28:33 +0530

    Recursion questions final commit.

--- Recursion/fibonacciSum/fibonacciSum.cc
+++ Recursion/fibonacciSum/fibonacciSum.cc
@@ -1,12 +1,13 @@
 #include<iostream>
 using namespace std;
+static int sum=0;
 void fibo(int n){
     static int a=0,b=1,c;
     if(n>0){
         c=a+b;
         a=b;
         b=c;
-        cout<<c<<" ";
+        sum+=c;
         fibo(n-1);
     }
 }
@@ -14,12 +15,12 @@ int main(){
     int n;
     cin>>n;
     if(n>2){
-        cout<<0<<" "<<1<<" ";
         fibo(n-2);
+        cout<<sum+1;
     }
     else if(n==1)
         cout<<0;
     else if(n==2)
-        cout<<0<<" "<<1;
+        cout<<1;
     return 0;
 }
